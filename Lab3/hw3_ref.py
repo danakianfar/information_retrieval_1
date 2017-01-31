7a85616f91d1c0a2a4def91042311573c5e5955f
Jose Gallego <jgalle29@gmail.com>
2017-01-31 13:25:39 +0100

    step 3 complete

--- Lab3/hw3_ref.py
+++ Lab3/hw3_ref.py
@@ -10,6 +10,7 @@ from itertools import count
 import query
 import os
 from scipy.sparse import dok_matrix
+from scipy.special import expit
 
 NUM_EPOCHS = 500
 
@@ -26,10 +27,6 @@ LISTWISE = 'listwise'
 # Cut-off level for NDCG metric
 ndcg_k = 10
 
-# TODO: Implement the lambda loss function
-def lambda_loss(output, lambdas):
-    raise "Unimplemented"
-
 # Calculates the best NDCG@k for query with r relevant documents and binary relevance labels
 def best_ndcg(r, k):
     if r == 0:
@@ -39,13 +36,21 @@ def best_ndcg(r, k):
     return b_rank, np.cumsum(b_rank, axis=0)
 
 # Store the logarithmic discount and normalization factor lists for faster NDCG computation
-disc_list, norm_list = best_ndcg(10,10)
+disc_list, norm_list = best_ndcg(1000,1000)
 disc_list = disc_list#.reshape((-1,1))
 
 # Calculates the NDCG@k for a rank with binary relevance labels assuming a query with r relevant documents
 def ndcg(rank, k, r = 1):
     return np.transpose(rank[:k]).dot(disc_list[:k+1]) / norm_list[r-1]
 
+# Calculates the delta on the NDCG@1000 when documents at positions i and j are swapped
+# id_i is the index in the labels list of the document in position i in the rank, sim. for id_j
+def delta_ndcdg(i, j, id_i, id_j, labels):
+    return (2**labels[id_i] - 2**labels[id_j]) * (disc_list[j] - disc_list[i]) * norm_list[int(np.sum(labels))]
+
+# TODO: Implement the lambda loss function
+def lambda_loss(output, lambdas):
+    return output * lambdas
 
 class LambdaRankHW:
 
@@ -58,11 +63,11 @@ class LambdaRankHW:
         self.measure_type = measure_type
 
     # train_queries are what load_queries returns - implemented in query.py
-    def train_with_queries(self, train_queries, num_epochs, val_queries):
+    def train_with_queries(self, train_queries, num_epochs, val_queries, S):
         res = []
         try:
             now = time.time()
-            for epoch in self.train(train_queries, val_queries):
+            for epoch in self.train(train_queries, val_queries, S):
                 if epoch['number'] % 1 == 0:
                     print("Epoch {} of {} took {:.3f}s".format(
                     epoch['number'], num_epochs, time.time() - now))
@@ -130,17 +135,19 @@ class LambdaRankHW:
         output_row_det = lasagne.layers.get_output(output_layer, X_batch,deterministic=True, dtype="float32")
 
         # TODO: Change loss function
-        # Point-wise loss function (squared error) - comment it out
-        loss_train = lasagne.objectives.squared_error(output,y_batch)
-        # Pairwise loss function - comment it in
-        # loss_train = lambda_loss(output,y_batch)
+        if self.measure_type == POINTWISE:
+            # Point-wise loss function (squared error) - comment it out
+            loss_train = lasagne.objectives.squared_error(output,y_batch)
+        else:
+            # Pairwise loss function - comment it in
+            loss_train = lambda_loss(output,y_batch)
 
         loss_train = loss_train.mean()
 
         # TODO: (Optionally) You can add regularization if you want - for those interested
-        # L1_loss = lasagne.regularization.regularize_network_params(output_layer,lasagne.regularization.l1)
-        # L2_loss = lasagne.regularization.regularize_network_params(output_layer,lasagne.regularization.l2)
-        # loss_train = loss_train.mean() + L1_loss * L1_reg + L2_loss * L2_reg
+        L1_loss = lasagne.regularization.regularize_network_params(output_layer,lasagne.regularization.l1)
+        L2_loss = lasagne.regularization.regularize_network_params(output_layer,lasagne.regularization.l2)
+        loss_train = loss_train.mean() + L1_loss * L1_reg + L2_loss * L2_reg
 
         # Parameters you want to update
         all_params = lasagne.layers.get_all_params(output_layer)
@@ -173,36 +180,43 @@ class LambdaRankHW:
         )
 
     # TODO: Implement the aggregate (i.e. per document) lambda function
-    def lambda_function(self,labels, scores):
-        pass
+    def lambda_function(self, labels, scores, S):
+        lambda_vec = np.zeros((len(labels),1))
+        order = np.argsort(-scores)
+        for ((w,l),_) in S.items():
+            lambda_wl = - expit( scores[l] - scores[w])
+            if self.measure_type == LISTWISE:
+                lambda_wl *= delta_ndcdg(np.where(order == w)[0][0], np.where(order == l)[0][0], w, l, labels)
+            lambda_vec[w,1] += lambda_wl
+            lambda_vec[l,1] -= lambda_wl
+        return lambda_vec
 
 
-    def compute_lambdas_theano(self,query, labels):
+    def compute_lambdas_theano(self,query, labels, S):
         scores = self.score(query).flatten()
-        result = self.lambda_function(labels, scores[:len(labels)])
+        result = self.lambda_function(labels, scores[:len(labels)], S)
         return result
 
-    def train_once(self, X_train, query, labels):
+    def train_once(self, X_train, query, labels, S):
 
-        # TODO: Comment out to obtain the lambdas
-        # lambdas = self.compute_lambdas_theano(query,labels)
-        # lambdas.resize((BATCH_SIZE, ))
-
-        # X_train.resize((BATCH_SIZE, self.feature_count),refcheck=False)
         # Alexandre L. correction
-        resize_value = BATCH_SIZE
-        if self.measure_type == POINTWISE:
-            resize_value = min(resize_value, len(labels))
+        resize_value = min(BATCH_SIZE, len(labels))
         X_train.resize((resize_value, self.feature_count), refcheck=False)
 
+        # TODO: Comment out to obtain the lambdas
 
-        # TODO: Comment out (and comment in) to replace labels by lambdas
-        #batch_train_loss = self.iter_funcs['train'](X_train, lambdas)
-        batch_train_loss = self.iter_funcs['train'](X_train, labels)
+        if self.measure_type != POINTWISE:
+            lambdas = self.compute_lambdas_theano(query,labels, S[query.get_qid()])
+            lambdas.resize((resize_value, ))
+            # TODO: Comment out (and comment in) to replace labels by lambdas
+            batch_train_loss = self.iter_funcs['train'](X_train, lambdas)
+        else:
+            batch_train_loss = self.iter_funcs['train'](X_train, labels)
+
         return batch_train_loss
 
 
-    def train(self, train_queries, val_queries):
+    def train(self, train_queries, val_queries, S):
         X_trains = train_queries.get_feature_vectors()
 
         queries = list(train_queries.values())
@@ -218,7 +232,7 @@ class LambdaRankHW:
                 random_index = random_batch[index]
                 labels = queries[random_index].get_labels()
 
-                batch_train_loss = self.train_once(X_trains[random_index],queries[random_index],labels)
+                batch_train_loss = self.train_once(X_trains[random_index],queries[random_index],labels, S)
                 batch_train_losses.append(batch_train_loss)
 
             avg_train_loss = np.mean(batch_train_losses)
@@ -251,10 +265,9 @@ def create_S_matrix(queries):
         S = dok_matrix((num_labels, num_labels), dtype=np.float32)
         labels = np.array(query.get_labels())
         ones_idx = np.where(labels == 1)
-        nonones_idx = np.where(labels != 1)
-
+        non_ones_idx = np.where(labels != 1)
         for i in ones_idx:
-            for j in nonones_idx:
+            for j in non_ones_idx:
                 S[i, j] = 1
         S_dict[query.get_qid()] = S
 
@@ -271,15 +284,14 @@ def experiment(n_epochs, measure_type, num_features, num_folds):
         print('\nLoading val queries')
         val_queries = query.load_queries(os.path.normpath('./HP2003/Fold%d/vali.txt' % fold), num_features)
 
-
-        print('Creating the S Matrix...')
+        print('Creating the S Matrix')
         S = create_S_matrix({**train_queries, **val_queries})
 
         # Creates a new ranker
         ranker = LambdaRankHW(num_features, measure_type)
 
         # Stores the statistics for each epoch
-        res = ranker.train_with_queries(train_queries, n_epochs, val_queries)
+        res = ranker.train_with_queries(train_queries, n_epochs, val_queries, S)
 
         # Saves the trained ranker
         res.append(ranker)
